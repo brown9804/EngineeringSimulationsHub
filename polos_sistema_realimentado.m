6fd335709f5da76c364d11171cc80e4a036bd887
Belinda Brown <belindabrownr04@gmail.com>
2020-01-22 17:07:21 -0600

    Add files via upload

---
+++ polos_sistema_realimentado.m
@@ -0,0 +1,28 @@
+%% Comprobación de todos los polos del sistema de control realimentado
+%% utilizando el polinomio característico
+s = tf('s'); % Definición de variable compleja.
+t = (0:1:60); % Definición de intervalo de tiempo.
+%% Caso 1 de funcion L(S) (14,5 <Kp1 < 100)
+kp1 = 20;
+Ls1 = kp1*(s^2 +2*s+4)/s*(s^2 +1.4*s+1)*(s+4)*(s+6);
+Myr1 =Ls1/(1+Ls1);
+step(Myr1,t);
+hold on;
+%% Caso 2 de la función L(S) (Kp2 = 4,46 a infinito).
+kp2 = 20;
+Ls2 = 2*kp2/(s-1)*(s^2 +10*s+29);
+Myr2 = Ls2/(1+Ls2);
+step(Myr2,t);
+hold on;
+
+
+legend('Caso 1 de funcion L(S) (14,5 <Kp1 < 100).','Caso 2 de la función L(S) (Kp2 = 4,46 a infinito).')
+
+figure;
+
+%% Diagrama de polo y ceros para los 3 casos
+pzmap(Myr1);
+hold on;
+pzmap(Myr2);
+
+legend('Caso 1 de funcion L(S) (14,5 <Kp1 < 100).','Caso 2 de la función L(S) (Kp2 = 4,46 a infinito).')
